00545175a0e61fa6ed90dfcac85dd1e6b12e1c63
Sergey Murin <sergeimurin29@gmail.com>
2023-02-27 14:46:09 +0300

    init webpack

---
+++ www.Frontend/src/hello.tsx
@@ -0,0 +1,4 @@
+import * as React from "react";
+
+export const Hello: React.FC = () => <h1>Hello from React!!</h1>;
+
---
+++ www.Frontend/src/index.tsx
@@ -0,0 +1,7 @@
+import { createRoot } from "react-dom/client";
+import {Hello} from "./hello";
+
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<Hello />);
